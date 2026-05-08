c79201dc34dfb4e762384f7109d235a968607cdc
Arthur Gonçalves <oliveraarthurrtx@gmail.com>
2026-05-08 15:56:46 -0300

    Firt commit in project

---
+++ src/Filme.java
@@ -0,0 +1,9 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+    //todo este codigo acima e o esboco do nosso filme
+}
